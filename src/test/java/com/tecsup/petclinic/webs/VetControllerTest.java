5602368a33cb64f7d99b15611d34a04e605f27f3
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2024-11-07 17:23:12 -0500

    comit deleted

--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -20,5 +20,32 @@ public class VetControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(VET_ID)));
     }
+    @Test
+    public void testDeleteVet() throws Exception {
+        String VET_NAME = "Dr. Brown";
+        int TYPE_ID = 2;
+        int OWNER_ID = 2;
+        String BIRTH_DATE = "1975-04-10";
+
+        Vet newVet = new Vet();
+        newVet.setName(VET_NAME);
+        newVet.setTypeId(TYPE_ID);
+        newVet.setOwnerId(OWNER_ID);
+        newVet.setBirthDate(BIRTH_DATE);
+
+        // Crear el veterinario
+        ResultActions mvcActions = mockMvc.perform(post("/vets")
+                        .content(om.writeValueAsString(newVet))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        String response = mvcActions.andReturn().getResponse().getContentAsString();
+        Integer id = JsonPath.parse(response).read("$.id");
+
+        // Eliminar el veterinario
+        mockMvc.perform(delete("/vets/" + id))
+                .andExpect(status().isOk());
+    }
 
 }
