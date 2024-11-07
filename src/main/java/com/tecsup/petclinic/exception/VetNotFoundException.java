23f45a44f0e7a73f24ee5391d2d6651166a4fc6a
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2024-11-07 16:33:45 -0500

    vet comit

---
+++ src/main/java/com/tecsup/petclinic/exception/VetNotFoundException.java
@@ -0,0 +1,22 @@
+package com.tecsup.petclinic.exception;
+
+/**
+ * 
+ * @author jgomezm
+ *
+ */
+public class VetNotFoundException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VetNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
