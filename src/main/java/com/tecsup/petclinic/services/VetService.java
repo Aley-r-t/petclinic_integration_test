0d035284b91057fff1889467817fa5249dcd8ba7
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-07 16:35:12 -0500

    Create Vetservice

---
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -0,0 +1,70 @@
+package com.tecsup.petclinic.services;
+
+import java.util.List;
+
+
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.VetNotFoundException;
+
+
+public interface VetService {
+
+    /**
+     *
+     * @param vet
+     * @return
+     */
+
+    Vet create(Vet vet);
+
+    /**
+     *
+     * @param vet
+     * @return
+     */
+
+    Vet update(Vet vet);
+
+    /**
+     *
+     * @param id
+     * @throws VetNotFoundException
+     */
+    void delete(Interger id) throws VetNotFoundException;
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    Vet findById(Integer id) throws VetNotFoundException;
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    List<Vet> findByName(String name);
+
+    /**
+     *
+     * @param typeId
+     * @return
+     */
+    List<Vet> findByTypeId(int typeId);
+
+    /**
+     *
+     * @param ownerId
+     * @return
+     */
+
+    List<Vet> findByOwnerId(int ownerId);
+
+    /**
+     *
+     * @return
+     */
+    List<Vet> findAll();
+
+}
