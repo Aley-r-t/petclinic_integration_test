9d5d058b1fbd2b971814f3cc489891efed270299
andres Huancahuari <sanchezmezaeduardo92@gmail.com>
2024-11-07 16:41:15 -0500

    primer commit de vet

--- src/main/java/com/tecsup/petclinic/entities/Vet.java
+++ src/main/java/com/tecsup/petclinic/entities/Vet.java
@@ -1,4 +1,25 @@
 package com.tecsup.petclinic.entities;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity(name= "vets")
+@Data
 
 public class Vet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(name= "type_id")
+    private Integer typeId;
+    @Column(name = "owner_id")
+    private int ownerId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "birth_date")
+    private Date birthDate;
+
 }
