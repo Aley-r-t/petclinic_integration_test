e0fbdc0f08c2478dc045e172fa194e631be6bbd8
andres Huancahuari <sanchezmezaeduardo92@gmail.com>
2024-11-07 17:30:55 -0500

    importaciones de VetControllerTest

--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -1,10 +1,22 @@
 package com.tecsup.petclinic.webs;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.tecsup.petclinic.entities.Vet;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -12,6 +24,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 public class VetControllerTest {
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
 
 
     @Test
